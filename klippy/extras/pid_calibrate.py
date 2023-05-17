e40226baf19e8634c580af47dee26a482e3a3775
Zeanon <thezeanon@gmail.com>
2023-05-18 00:29:56 +0200

    Update pid_calibrate.py

--- klippy/extras/pid_calibrate.py
+++ klippy/extras/pid_calibrate.py
@@ -6,8 +6,6 @@
 import math, logging
 from . import heaters
 
-# -*- coding: utf-8 -*-
-
 class PIDCalibrate:
     def __init__(self, config):
         self.printer = config.get_printer()
@@ -43,7 +41,7 @@ class PIDCalibrate:
         Kp, Ki, Kd = calibrate.calc_pid()
         logging.info("Autotune: final: Kp=%f Ki=%f Kd=%f", Kp, Ki, Kd)
         gcmd.respond_info(
-            "PID parameters for %.2f°C: pid_Kp=%.3f pid_Ki=%.3f pid_Kd=%.3f\n"
+            "PID parameters for %.2fC: pid_Kp=%.3f pid_Ki=%.3f pid_Kd=%.3f\n"
             "Profile: [%s]\n"
             "The SAVE_CONFIG command will update the printer config file\n"
             "with these parameters and restart the printer."
