7dd33e4a243d6082a7b9249a2aa228dc62e1c08e
Zeanon <thezeanon@gmail.com>
2023-08-01 15:38:01 +0200

    Update controller_temperature_fan.py

--- klippy/extras/controller_temperature_fan.py
+++ klippy/extras/controller_temperature_fan.py
@@ -107,6 +107,8 @@ class ControllerTemperatureFan:
         speed_time = read_time + self.speed_delay
         self.next_speed_time = speed_time + 0.75 * MAX_FAN_TIME
         self.last_speed_value = value
+        gcode = self.printer.lookup_object('gcode')
+        gcode.repond_info("%f" % value)
         self.fan.set_speed(speed_time, value, False)
     def callback(self, eventtime):
         speed = self.idle_speed
