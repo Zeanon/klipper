4fc9644c47dd3281ebefe85d734d86c8c5e39ca8
Zeanon <thezeanon@gmail.com>
2023-08-26 18:38:33 +0200

    Update homing.py

--- klippy/extras/homing.py
+++ klippy/extras/homing.py
@@ -68,8 +68,9 @@ class HomingMove:
     def homing_move(self, movepos, speed, probe_pos=False,
                     triggered=True, check_triggered=True):
         # Notify start of homing/probing move
+        test = self
         logging.info("1")
-        self.printer.send_event("homing:homing_move_begin", self)
+        self.printer.send_event("homing:homing_move_begin", test)
         logging.info("1")
         # Note start location
         self.toolhead.flush_step_generation()
