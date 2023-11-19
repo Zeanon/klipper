ecc5c7cc0fd84fb6c7dfdb7f661148f0559ce143
Zeanon <thezeanon@gmail.com>
2023-11-19 22:56:07 +0100

    Update stepper_enable.py

--- klippy/extras/stepper_enable.py
+++ klippy/extras/stepper_enable.py
@@ -36,25 +36,18 @@ class StepperEnablePin:
         if self.mcu_enable is not None:
             self.enable_count -= 1
             if not self.enable_count:
-                toolhead = self.printer.lookup_object('toolhead')
-                toolhead.register_lookahead_callback(
-                    lambda print_time: self._set_pin(print_time, 0))
+                self._set_pin(print_time, 0)
     def _set_pin(self, print_time, value, is_resend=False):
         if (value == self.last_value
                 and not is_resend):
             return
 
-        print_time = max(print_time, self.last_print_time + PIN_MIN_TIME)
         self.mcu_enable.set_digital(print_time, value)
-        if value != 0:
-            self.last_value = 1
-            self.last_print_time = print_time
-            if self.resend_interval and self.resend_timer is None:
-                self.resend_timer = self.reactor.register_timer(
-                    self._resend_current_val, self.reactor.NOW)
-        else:
-            self.last_value = 0
-            self.last_print_time = print_time
+        self.last_value = value
+        self.last_print_time = print_time
+        if self.resend_interval and self.resend_timer is None:
+            self.resend_timer = self.reactor.register_timer(
+                self._resend_current_val, self.reactor.NOW)
 
     def _resend_current_val(self, eventtime):
         if self.last_value == 0:
