574a40a9d862dda80df68b9c33fc3610a3a47b47
Zeanon <thezeanon@gmail.com>
2024-01-11 19:04:10 +0100

    Update fan.py

--- klippy/extras/fan.py
+++ klippy/extras/fan.py
@@ -49,11 +49,20 @@ class Fan:
 
         self.name = config.get_name().split()[-1]
         self.num_err = 0
-        self.max_err = config.getint("max_err", 3, minval=0)
-        self.min_rpm = config.getint("min_rpm", 0, minval=0)
-        if self.min_rpm > 0 and self.tachometer._freq_counter is None:
+        self.min_rpm = config.getint("min_rpm", None, minval=0)
+        self.max_err = config.getint("max_error", None, minval=0)
+        if (self.min_rpm is not None
+                and self.min_rpm > 0
+                and self.tachometer._freq_counter is None):
             raise config.error(
                 "'tachometer_pin' must be specified before enabling `min_rpm`")
+        if self.max_err is not None and self.min_rpm is None:
+            raise config.error(
+                "'min_rpm' must be specified before enabling `max_error`")
+        if self.min_rpm is None:
+            self.min_rpm = 0
+        if self.max_err is None:
+            self.max_err = 3
 
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
         # Register callbacks
