4b403fe3c1af8ebe0da4e7d8842bb491a7e21714
Zeanon <thezeanon@gmail.com>
2024-03-24 01:28:47 +0100

    Update heaters.py

--- klippy/extras/heaters.py
+++ klippy/extras/heaters.py
@@ -905,8 +905,6 @@ class PrinterHeaters:
             eventtime = reactor.pause(eventtime + 1.)
     def set_temperature(self, heater, temp, wait=False, gcmd=None):
         if not heater.enabled:
-            logging.info("gcmd")
-            logging.info(gcmd)
             if gcmd is not None:
                 gcmd.respond_info("Heater [%s] is disabled due to an "
                                   "accelerometer being connected."
